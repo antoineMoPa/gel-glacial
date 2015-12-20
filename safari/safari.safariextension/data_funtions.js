40c4cd6e0fdb606ebafe953f8ca2ff1d930cb3a2
Donavan <mart.donavan@gmail.com>
2015-12-20 08:59:39 -0500

    Stable version of Loading ponderation grid button

--- safari/safari.safariextension/data_funtions.js
+++ safari/safari.safariextension/data_funtions.js
@@ -1,3 +1,5 @@
+/* Version 0.2 */
+
 var data_ponderation;
 var btn_ponderation;
 var pond_grid;
@@ -15,7 +17,7 @@ function addon_ponderation(){
     btn_ponderation.classList.add("gel-glacial");
     btn_ponderation.onclick = get_all_ponderation;
 
-	// Btns div
+	// Btn div
     var div_btn = document.createElement("div");
 	div_btn.id = "btn_ponderation";
     div_btn.classList.add("gel-glacial-bar");
@@ -28,13 +30,20 @@ function addon_ponderation(){
 // Get Ponderation datas from url ponderation
 function get_all_ponderation(){
 
+    // Change Note's div id
     document.getElementById("gridContainer4").id = "gridNotes";
-
+    document.getElementById("Loader").id = "LoaderNotes";
+    
+    // Disable btn for loading
     btn_ponderation.disabled = true;
-    btn_ponderation.innerHTML = "Load datas...";
+    btn_ponderation.innerHTML = "Telechargement...";
+    
+    // Set url to get data ponderation
     var url = window.location.href;
     url = url.replace("notesEtu.php", "ponderation.php");
-    $("#ponderation_grid").load(url,function(){
+    
+    // Get all data needed from ponderation page
+    $("#ponderation_grid").load(url ,function(){
         data_ponderation = data;
 		for (i = 0; i < data.length-1; i++) {
 			for (j = 0; j < countProperties(data[i])-1; j++) {
@@ -46,12 +55,59 @@ function get_all_ponderation(){
 					}
 				}
 			}
-		}
-        btn_ponderation.onclick = show_hide_ponderation;
-        btn_ponderation.disabled = false;
-        btn_ponderation.innerHTML = "Cacher grille des ponderations";
-        document.getElementById('ponderation_grid').setAttribute('style','display: block');
-	});
+		};
+        // Add listener when loading is complet
+        observe_loading();
+    });
+}
+
+// Observer Loading Page 0_0
+function observe_loading(){
+
+    // Observe when page will be all loaded (necessary)
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutationRecord) {
+        
+            // At this point some cell's data remaining
+            observe_cells();
+            //console.log('Page ponderation finish loading');
+        });
+    });
+    
+    // Apply observer on target
+    document.getElementById('Loader').setAttribute('style','visibility: visible;');
+    var target = document.getElementById('Loader');
+    observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+}
+
+// Observe Grid Finish Loading 0_0
+function observe_cells(){
+
+    // Observe when page will be all loaded (necessary)
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutationRecord) {
+            // Keep only grid
+            keep_grid_only();
+            // Reset btn
+            btn_ponderation.onclick = show_hide_ponderation;
+            btn_ponderation.disabled = false;
+            // Hide grid
+            show_hide_ponderation();
+            //console.log('Grid ponderation finish loading');
+        });
+    });
+    
+    // Apply target
+    jQuery('#gridContainer4').find('.dojoxGridMasterView').attr('style','height: 0px;');
+    jQuery('#gridContainer4').find('.dojoxGridMasterView').attr('id','gridPonderation');
+    var target = document.getElementById('gridPonderation');
+    observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+}
+
+// Keep grid only from loaded page
+function keep_grid_only(){
+    var grid_only = jQuery('#gridContainer4');
+    jQuery('#ponderation_grid').html(grid_only.html());
 }
 
 // Create Ponderation Grid
@@ -61,9 +117,6 @@ function create_div_pond_grid(){
 	
 	// Append grid
 	document.getElementsByTagName('body')[0].appendChild(pond_grid);
-    
-	// Debug in console
-	console.log("Ponderation Grid created");
 }
 
 // Show hide grid div && create it if not exist
@@ -75,7 +128,6 @@ function show_hide_ponderation(){
         btn_ponderation.innerHTML = "Afficher Grille des ponderations";
         document.getElementById('ponderation_grid').setAttribute('style','display: none');
 
-    
     //Hide
     }else{
         btn_ponderation.innerHTML = "Cacher grille des ponderations";

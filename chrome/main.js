9622e89ba843afa0066cbfdcb0920c438515c2ad
Felix Martel <lixtelnis@gmail.com>
2015-12-25 14:30:56 -0500

    Ported firefox data metadata structure to chrome

--- chrome/main.js
+++ chrome/main.js
@@ -1,38 +1,17 @@
-var addons = [];
+var addons = {};
 
 function chrome_load_addons(){
-	//TODO need to find all addons in the addons folder and load them
-	//these fields should be in the addon file and more modular (lib)
-	addons.push({
-		"regex_path" : /.*\.usherbrooke\.ca\/.*\/notesEtu\.php/,
-		"addon_path" : ["./lib/external/jquery-2.1.4.min.js", "./addons/marks.js", "./addons/marks_weighting.js"],
-		"style_path" : "./style.css",
-	});
+    addons = get_addons();
 }
 
 function chrome_exec_addons(){
-	//Set listener to a url and execute the addon when matched
-	
-	
-	//Construct all addons mod
-	addons.forEach(function(addon){
-		if(addon.regex_path.test(window.location.href)){
-			addon.addon_path.forEach(function(addon_path) {
-				inject_script(addon_path);
+	Object.keys(addons).forEach(function(key){
+		var addon = addons[key];
+		if(addon.regexPath.test(window.location.href)){
+			addon.scriptFiles.forEach(function(script_path) {
+				inject_script(script_path);
 			});
 		}
-		/*
-		pageMod.PageMod({
-			"include" : addon.regex_path,
-			"contentScriptFile" : [
-				"./lib/external/jquery-2.1.4.min.js",
-				addon.addon_path
-			],
-			"contentStyleFile" : addon.style_path,
-			"onAttach" : function(worker){
-				worker.port.emit("exec");
-			}
-		})*/
 	})
 }
 
---
+++ chrome/metadata/Addons.js
@@ -0,0 +1,35 @@
+//get_addons has a global scope
+var get_addons = (function() {
+	var addons = {
+		"marks" : {
+			"regexPath" : /.*\.usherbrooke\.ca\/.*\/notesEtu\.php/,
+			"scriptFiles" : ["jquery.js", "marks.js", "marks_weighting.js"],
+			"styleFiles" : ["style.css"]
+		}
+	}
+
+	//paths is relative to data/ folder
+	var paths = {
+		"jquery.js" : "./lib/external/jquery-2.1.4.min.js",
+		"marks.js" : "./addons/marks.js",
+		"marks_weighting.js" : "./addons/marks_weighting.js",
+		"style.css" : "./style.css",	
+	}
+
+	return function(){
+		var new_addons = {};
+		
+		Object.keys(addons).forEach(function(key){
+			var addon = addons[key];
+			new_addons[key] = {};
+			new_addons[key].regexPath = addon.regexPath;
+			new_addons[key].scriptFiles = addon.scriptFiles.map(function(script){
+				return paths[script];
+			});
+			new_addons[key].styleFiles = addon.styleFiles.map(function(style){
+				return paths[style];
+			});
+		});
+		return new_addons;
+	}
+})();

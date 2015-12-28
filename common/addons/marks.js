e878c481c7c7fa6eefd5b366e8a3a19968d42302
Felix Martel <lixtelnis@gmail.com>
2015-12-27 19:49:36 -0500

    Option de retour aux valeurs d'origine de la grille

--- common/addons/marks.js
+++ common/addons/marks.js
@@ -1,15 +1,13 @@
-var data;
+var marks_grid;
+var button;
 
 function addon_marks(){
-	//gather all important data
-	data = get_data();
-	
 	//Create the addon div
 	var div = document.createElement("div");
 	div.classList.add("gel-glacial-bar");
 	
 	//Create the button for percent convertion
-	var button = document.createElement("button");
+	button = document.createElement("button");
 	button.innerHTML = "Convertir en %"
 	button.classList.add("gel-glacial");
 	
@@ -23,20 +21,46 @@ function addon_marks(){
 
 //TODO translation
 function show_percent_mark(){
+	if(typeof marks_grid === 'undefined'){
+		//gather all important marks_grid
+		marks_grid = get_marks_grid();
+	}
+
 	//Change marks in the grid
 	var table_rows = document.querySelectorAll(".dojoxGridMasterView .dojoxGridRow");
-	for(var i = 0; i < data.rows.length; i++){
+	for(var i = 0; i < marks_grid.rows.length; i++){
 		var row = table_rows[i].querySelectorAll("td.dojoxGridCell");
 		
 		//Single marks
-		for(var j = 0; j < data.rows[i].cells.length; j++){
-			set_percent_mark_cell_value(data.rows[i].cells[j], row, j+3);
+		for(var j = 0; j < marks_grid.rows[i].cells.length; j++){
+			set_percent_mark_cell_value(marks_grid.rows[i].cells[j], row, j+3);
 		}
 		//TÉ column
-		set_percent_mark_cell_value(data.rows[i].total, row, 2);
+		set_percent_mark_cell_value(marks_grid.rows[i].total, row, 2);
 	}
 	
 	//TODO change mark at last row to substract missing marks' weight
+	
+	button_toggle();
+}
+
+function show_original_mark(){
+	//Change marks in the grid
+	var table_rows = document.querySelectorAll(".dojoxGridMasterView .dojoxGridRow");
+	for(var i = 0; i < marks_grid.rows.length; i++){
+		var row = table_rows[i].querySelectorAll("td.dojoxGridCell");
+		
+		//Single marks
+		for(var j = 0; j < marks_grid.rows[i].cells.length; j++){
+			set_mark_cell_value(marks_grid.rows[i].cells[j], row, j+3);
+		}
+		//TÉ column
+		set_mark_cell_value(marks_grid.rows[i].total, row, 2);
+	}
+	
+	//TODO change mark at last row to substract missing marks' weight
+	
+	button_toggle();
 }
 
 function set_percent_mark_cell_value(mark, table_row, table_cell_id){
@@ -48,11 +72,20 @@ function set_percent_mark_cell_value(mark, table_row, table_cell_id){
 	}
 }
 
+function set_mark_cell_value(mark, table_row, table_cell_id){
+	if(mark != null && mark.mark != null && mark.weight != null){
+		var value = (mark.mark).toFixed(1);
+		set_cell_value(value, table_row, table_cell_id);
+	} else {
+		set_cell_value("", table_row, table_cell_id);
+	}
+}
+
 function set_cell_value(value, table_row, table_cell_id){
 	table_row[table_cell_id].innerHTML = value;
 };
 
-function get_data(){
+function get_marks_grid(){
 	//There are variables needed in the notesEtu.php file
 	//we can't acces it here so we need to set it as a body attribute and capture it later
 	var script = document.createElement("script");
@@ -178,6 +211,22 @@ function build_mark_obj(obj){
 	
 	//if something is wrong
 	return null;
+}
+
+function button_toggle(){
+	if(button.innerHTML == "Convertir en %"){
+		button.innerHTML = "Notes d'origine"
+		button.classList.add("gel-glacial");
+	
+		//Set the callback of the button
+		button.onclick = show_original_mark;
+	} else{
+		button.innerHTML = "Convertir en %"
+		button.classList.add("gel-glacial");
+	
+		//Set the callback of the button
+		button.onclick = show_percent_mark;
+	}
 }
 
 function exec(){
